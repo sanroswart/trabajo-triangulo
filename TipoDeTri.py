c3004227e129ea0f2bbcb7e708e379f9d843920c
sanros <montascito11@gmail.com>
2021-11-18 16:31:07 -0500

    tipos de triangulos

--- TipoDeTri.py
+++ TipoDeTri.py
@@ -4,6 +4,7 @@ random.seed()
 L1 = random.randint(1,3)
 L2 = random.randint(1,3)
 L3 = random.randint(1,3)
+
 if L1!=L2!=L3:
     print('el Triangulo es Escaleno')
 elif L1!=L2==L3:
@@ -12,6 +13,5 @@ elif L1==L2==L3:
     print('el Triangulo es Equilatero')
 else:
     print('numero de lados no validos')
-while L1 < 3:
-    print()
+
 
